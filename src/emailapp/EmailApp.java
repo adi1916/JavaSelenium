4a2fd40bb17523aa4cc8bff78293de4604a22141
Adi Dinu <adrianflorin.dinu@gmail.com>
2021-01-01 23:17:07 +0200

    2

--- src/emailapp/EmailApp.java
+++
@@ -1,9 +0,0 @@
-package emailapp;
-
-public class EmailApp {
-    public static void main(String[] args) {
-        Email em1 = new Email("Adi", "Dinu");
-
-        System.out.println(em1.showInfo());
-    }
-}
