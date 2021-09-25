0703dfcfce7433ceed9eba0579a1253f7d911d7b
Adi Dinu <adrianflorin.dinu@gmail.com>
2021-09-25 19:27:40 +0300

    Changes 25 Sep

--- src/Testcases/course_examples/Loginapplication.java
+++ src/Testcases/course_examples/Loginapplication.java
@@ -1,7 +1,7 @@
-package Testcases;
+package Testcases.course_examples;
 
-import objectrepository.RediffHomepage;
-import objectrepository.RediffLoginpage;
+import objectRepository.course_examples.RediffHomepage;
+import objectRepository.course_examples.RediffLoginpage;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +14,7 @@ public class Loginapplication {
 	@Test
 	public void Login()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Adi\\IdeaImportModules\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Adi\\IdeaImportModules\\chromedriver93\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
 
--- src/objectRepository/course_examples/RediffLoginpage.java
+++ src/objectRepository/course_examples/RediffLoginpage.java
@@ -1,4 +1,4 @@
-package course_PageObjectModel;
+package objectRepository.course_examples;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,27 +15,31 @@ public class RediffLoginpage {
    By username=By.xpath(".//*[@id='login1']");
    By Password=By.name("passwd");
    By go=By.name("proceed");
-   By home=By.linkText("Home");
-   By newAccount = By.linkText("Create a new account");
+   By home=By.linkText("rediff.com");
+   
    
    public WebElement Emaild()
    {
 	   return driver.findElement(username);
    }
+         
    public WebElement Password()
    {
 	   return driver.findElement(Password);
    }
+   
    public WebElement submit()
    {
 	   return driver.findElement(go);
    }
+   
    public WebElement Home()
    {
 	   return driver.findElement(home);
    }
-   public WebElement newAccount()
-    {
-        return driver.findElement(newAccount);
-    }
+	
+
+	
+	
+	
 }
