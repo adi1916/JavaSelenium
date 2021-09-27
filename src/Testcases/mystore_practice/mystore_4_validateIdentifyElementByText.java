2b1e13b33a9d9f599fb07edde75af4b89604ebf7
adi1916 <adrianflorin.dinu@gmail.com>
2021-09-27 21:47:30 +0300

    27-Sep: some example of location webelement

---
+++ src/Testcases/mystore_practice/mystore_4_validateIdentifyElementByText.java
@@ -0,0 +1,30 @@
+package Testcases.mystore_practice;
+
+import objectRepository.mystore_practice.mystoreIndexPage;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class mystore_4_validateIdentifyElementByText {
+
+	@Test
+	public void TC1_validateIdentifyElementByText()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\IdeaImportModules\\chromedriver93\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+
+		mystoreIndexPage idx=new mystoreIndexPage(driver);
+		idx.DressesCategory().click();
+		driver.findElement(By.xpath("//*[contains(text(),'Printed Chiffon Dress')]")).click();
+		//driver.findElement(By.xpath("//ul[contains(@class,'product_list')]/li[2]/div/div/div/a/img")).click();
+		//String returnedProduct = driver.findElement(By.xpath("//p[@id='product_reference']/span")).getText();
+		//assertTrue(returnedProduct.contains("demo_4"));
+	}
+
+
+}
