aa333dc94257896a29f75ee5ebb47874c167f735
adi1916 <adrianflorin.dinu@gmail.com>
2021-11-07 21:41:08 +0200

    07-Nov: Frames

---
+++ src/Testcases/course_examples/course_10_assignmentFrames.java
@@ -0,0 +1,26 @@
+package Testcases.course_examples;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class course_10_assignmentFrames {
+
+	@Test
+	public void TC10_assignmentFrames() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\IdeaImportModules\\chromedriver95\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/");
+		driver.findElement(By.linkText("Nested Frames")).click();
+
+		driver.switchTo().frame(0);
+		driver.switchTo().frame(1);
+		System.out.println( driver.findElement(By.id("content")).getText());
+	}
+}
